27ada15c945a218ceee01a786f6e2d81605c4ee1
Lan <lan@libero.it>
2020-09-17 12:30:36 +0200

    First modification from 2.0.5.x to 2.0.6.x - Not Working - Not Tested

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -165,7 +165,7 @@
   #define THERMAL_PROTECTION_PERIOD 40        // Seconds
   #define THERMAL_PROTECTION_HYSTERESIS 4     // Degrees Celsius
 
-  //#define ADAPTIVE_FAN_SLOWING              // Slow part cooling fan if temperature drops
+  #define ADAPTIVE_FAN_SLOWING              // Slow part cooling fan if temperature drops //Lan
   #if BOTH(ADAPTIVE_FAN_SLOWING, PIDTEMP)
     //#define NO_FAN_SLOWING_IN_PID_TUNING    // Don't slow fan speed during M303
   #endif
@@ -182,8 +182,10 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD 20                // Seconds
-  #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
+  //#define WATCH_TEMP_PERIOD 20                // Seconds
+  //#define WATCH_TEMP_INCREASE 2               // Degrees Celsius
+  #define WATCH_TEMP_PERIOD 40                // Seconds // Lan (80)
+  #define WATCH_TEMP_INCREASE 2               // Degrees Celsius //Lan (4)
 #endif
 
 /**
@@ -450,7 +452,8 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
+
+#define E0_AUTO_FAN_PIN 7
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Marlin
-Marlin Sharebot NG (My configuration)
+# MySharebotNg
+
+This is my first approach to Git and project sharing.
+
+This work for me!
+
+Based on first Sharebot NG firmware https://github.com/Sharebot3D/Marlin
+
+The original PSU was dead, I've changed it with a FTX PSU (from a dead 1U server ) I've implemented the PSU_CONTROL
+
+I've added a filament runout sensor.
+
+I'm try to add a bltouch sensor.
+
+
